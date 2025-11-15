6af95d5373f5a54a13753a6d0406855c42d2c00b
TarekAbdo223 <tarekk.abdo22@gmail.com>
2025-11-16 00:23:31 +0200

    make a test request

--- App.tsx
+++ App.tsx
@@ -1,22 +1,62 @@
 import axios from "axios";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Alert,
+  Button,
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [books, setBooks] = useState<any[]>([]);
+
   const endpointURL = "https://69186e1521a96359487003a9.mockapi.io/books";
 
   const getListOfBooks = async () => {
     const response = await axios.get(endpointURL);
-    console.log(response.data);
+    console.log(JSON.stringify(response.data, null, 3));
+    setBooks(response.data);
+  };
+
+  const postBookDetails = async () => {
+    try {
+      const response = await axios.post(endpointURL, {
+        name_of_author: "tarek abdo",
+        price_of_book: 100,
+        cover: "https://via.placeholder.com/150",
+        email_of_seller: "tarekabdo@gmail.com",
+      });
+      Alert.alert("Book posted successfully");
+    } catch (error) {
+      Alert.alert("Error posting book");
+    }
   };
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text onPress={getListOfBooks}>
         Open up App.tsx to start working on your app!
       </Text>
-      <StatusBar style="auto" />
-    </View>
+      <Button title="Post Book" onPress={postBookDetails} />
+      <FlatList
+        data={books}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <Text>{item.name_of_author}</Text>
+              <Text>{item.price_of_book}</Text>
+              <Image source={{ uri: item.cover }} style={styles.image} />
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -26,5 +66,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  image: {
+    width: 100,
+    height: 100,
   },
 });
