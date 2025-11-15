0cce16b58ec601b8eaa1bba81620f28660e8eb02
TarekAbdo223 <tarekk.abdo22@gmail.com>
2025-11-15 14:24:56 +0200

    make the get function of the list

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const endpointURL = "https://69186e1521a96359487003a9.mockapi.io/books";
+
+  const getListOfBooks = async () => {
+    const response = await axios.get(endpointURL);
+    console.log(response.data);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text onPress={getListOfBooks}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
