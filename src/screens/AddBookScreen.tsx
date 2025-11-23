c404f44151765947135e870bea0d5c82aea4dcf1
TarekAbdo223 <tarekk.abdo22@gmail.com>
2025-11-23 23:00:17 +0200

    added the inputs

--- src/screens/AddBookScreen.tsx
+++ src/screens/AddBookScreen.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import React from "react";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import AppTextInput from "../components/AppTextInput";
 
 const AddBookScreen = ({ onPress }) => {
   return (
@@ -11,10 +12,30 @@ const AddBookScreen = ({ onPress }) => {
         color="#E81D1D"
         onPress={onPress}
       />
+      <View style={styles.body}>
+        <Text style={styles.title}>Book Details</Text>
+        <AppTextInput placeholder="Book Name" />
+        <AppTextInput placeholder="Author Name" />
+        <AppTextInput placeholder="Cover Image" />
+        <AppTextInput placeholder="Book Price" />
+      </View>
     </SafeAreaView>
   );
 };
 
 export default AddBookScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  body: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    paddingHorizontal: 15,
+    paddingTop: 30,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginBottom: 20,
+  },
+});
